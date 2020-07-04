8f1a7663de926bbe9096cad71d260a26e5fdaa19
Jay Gupta <jay002@e.ntu.edu.sg>
2020-07-04 08:40:29 +0800

    Added Database Support

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 import gym
 import json
 import datetime as dt
-
 from stable_baselines.common.vec_env import DummyVecEnv
 from env.WarehouseEnv import WarehouseEnv
+import pymongo
+from pymongo import MongoClient
+from data.config import USERNAME, PASSWORD, DATABASE_NAME
+
+## Database Configuration ##
+connectionURL = "mongodb+srv://" + USERNAME + ":" + PASSWORD + \
+    "@m0-cluster.vxrgp.mongodb.net/" + DATABASE_NAME + "?retryWrites=true&w=majority"
+
+cluster = MongoClient(connectionURL)
+db = cluster["warehouse"]
+collection = db["schedule"]
+## Database Configuration ##
 
 # The algorithms require a vectorized environment to run
 env = DummyVecEnv([lambda: WarehouseEnv()])
@@ -12,18 +23,13 @@ obs = env.reset()
 
 print("")
 
-# Inserting package at Index 1
-action = [[1, 1]]
-obs, rewards, done, info = env.step(action)
-env.render()
-
-# Inserting package at Index 20
-action = [[20, 1]]
-obs, rewards, done, info = env.step(action)
-env.render()
+for package in collection.find():
+    action = [[package['packageID'], 1]]
+    obs, rewards, done, info = env.step(action)
+    env.render()
 
 # Withdrawing package at Index 1
-action = [[1, 0]]
+action = [[5, 0]]
 obs, rewards, done, info = env.step(action)
 env.render()
 
